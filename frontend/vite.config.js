c328bb07f64c511d6c2abfd5a490ac40ab604323
Saalim Aqueel <aqueel192@gmail.com>
2024-09-21 11:05:30 +0530

    fixes for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,24 +4,24 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://pendrop-backend.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        configure: (proxy, options) => {
-          proxy.on('error', (err, req, res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://pendrop-backend.onrender.com',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     ws: true,
+    //     configure: (proxy, options) => {
+    //       proxy.on('error', (err, req, res) => {
+    //         console.log('proxy error', err);
+    //       });
+    //       proxy.on('proxyReq', (proxyReq, req, res) => {
+    //         console.log('Sending Request to the Target:', req.method, req.url);
+    //       });
+    //       proxy.on('proxyRes', (proxyRes, req, res) => {
+    //         console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+    //       });
+    //     },
+    //   },
+    // },
   },
 });
