39371fe336aabd0166ec5521b66c4a00e0c0fcdf
Saalim Aqueel <aqueel192@gmail.com>
2024-09-21 10:01:09 +0530

    fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://pendrop-backend.onrender.com',
         changeOrigin: true,
         secure: false,
         ws: true,
